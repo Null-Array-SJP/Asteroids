2f14ff1be58a257298aab6117167ff950d62209e
Null-Array-SJP <bmorozovskyy@gmail.com>
2026-09-09 14:11:19 -0400

    added constants, updated main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import constants
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 
 if __name__ == "__main__":
