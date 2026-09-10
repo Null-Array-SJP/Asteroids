9388ddba6edab6adfae820c6c271cddd7b95ba19
Null-Array-SJP <bmorozovskyy@gmail.com>
2026-09-10 19:33:29 -0400

    added screen logic

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 import constants
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280")
     print("Screen height: 720")
