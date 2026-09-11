b8c5f208a166b7816c3b1c29e325b189bee2adb7
Null-Array-SJP <bmorozovskyy@gmail.com>
2026-09-11 16:59:53 -0400

    added ability to turn

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
