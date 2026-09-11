624cc5a5fbc87fb5154bc4612c50dd6b97966c2c
Null-Array-SJP <bmorozovskyy@gmail.com>
2026-09-11 17:47:04 -0400

    added groups and updated main.py to reflect

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,8 +22,9 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
